ed45ce7e6ec6c11bfc4c68c9258b001d1b2b1db0
DawidWit <135219147+DawidWit@users.noreply.github.com>
2025-06-02 11:22:22 +0200

    changes in queueing system

--- services/product-service/queues/scraperQueue.js
+++ services/product-service/queues/scraperQueue.js
@@ -1,18 +1,21 @@
-const Redis = require("ioredis");
 const { SCRAPER_QUEUE_NAME } = require("../config/source");
-const redis = new Redis("redis://localhost:6379");
+const redis = require("../services/redisService");
 
 async function enqueueScrapeJob(item) {
+  try {
+    if (!item || !item._id) throw new Error("Invalid item passed");
+    const temp = {
+      id: item._id.toString(),
+      item: item.item,
+      source: item.source,
+      currency: item.currency,
+      targetPrice: item.targetPrice,
+      active: item.active,
+    };
+    await redis.lpush(SCRAPER_QUEUE_NAME, JSON.stringify(temp));
+  } catch (error) {}
   console.log(item);
-  const temp = {
-    id: item._id.toString(),
-    item: item.item,
-    source: item.source,
-    currency: item.currency,
-    targetPrice: item.targetPrice,
-    active: item.active,
-  };
-  await redis.lpush(SCRAPER_QUEUE_NAME, JSON.stringify(temp));
+
   console.log("QUEUED");
 }
 
